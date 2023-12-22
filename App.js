2da0188f4712e15d7b53745dee598901d65aa967
AlessandroSalomonMac <>
2023-12-22 15:26:59 +0100

    ende 87 höhe und breite in Prozent angeben

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StyleSheet, View } from 'react-native';
 import React, { Component } from 'react';
 
-export default class App extends Component{
-  render(){
+export default class App extends Component {
+  render() {
     return (
       <View style={styles.container}>
-        <View style={{width: 150, height: 150, backgroundColor: 'blue'}}/>
-        <View style={{width: 150, height: 150, backgroundColor: 'red'}}/>
-        <View style={{width: 150, height: 150, backgroundColor: 'green'}}/>
-        <View style={{width: 150, height: 150, backgroundColor: 'gray'}}/>
+        <View style={{ width: '100%', height: 150, backgroundColor: 'blue' }} />
+        <View style={{ width: '80%', height: 150, backgroundColor: 'red' }} />
+        <View style={{ width: '60%', height: 150, backgroundColor: 'green' }} />
+        <View style={{ width: '40%', height: 150, backgroundColor: 'gray' }}>
+          <View style={{ width: '60%', height: '60%', backgroundColor: 'darkgreen' }} />
+        </View>
       </View>
     );
   }
